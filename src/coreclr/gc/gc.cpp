8b6c8d5396a47a33d101eefabced24a0442b3574
Timur Mustafin <t.mustafin@partner.samsung.com>
2025-05-12 20:49:58 +0300

    Fix ClearMarked() for not DOUBLY_LINKED_FL case

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -4880,11 +4880,7 @@ public:
     // Now we set more bits should actually only clear the mark bit
     void ClearMarked()
     {
-#ifdef DOUBLY_LINKED_FL
         RawSetMethodTable ((MethodTable *)(((size_t) RawGetMethodTable()) & (~GC_MARKED)));
-#else
-        RawSetMethodTable (GetMethodTable());
-#endif //DOUBLY_LINKED_FL
     }
 
 #ifdef DOUBLY_LINKED_FL
